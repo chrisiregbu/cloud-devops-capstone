7d5684815eea7e4275f43a510d71ee9864646a14
chrisiregbu <ciregbu@gmail.com>
2021-11-15 22:59:41 +0000

    Fixing angular-related linting issues

--- src/app/flexi-title-case.pipe.ts
+++ src/app/flexi-title-case.pipe.ts
@@ -7,15 +7,18 @@ export class FlexiTitleCasePipe implements PipeTransform {
 
   transform(value: string, upperOrLower: string): string {
 
-    if (upperOrLower !== 'upper' && upperOrLower !== 'lower') 
+    if (upperOrLower !== 'upper' && upperOrLower !== 'lower') {
       return value;
-    
-    let words:string[] = value.split(' ');
-    for (var i = 0; i < words.length; i++) {
-      if (upperOrLower === 'upper')
+    }
+
+    const words: string[] = value.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (upperOrLower === 'upper') {
         words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-      else 
+      }
+      else {
         words[i] = words[i].charAt(0).toLowerCase() + words[i].slice(1).toUpperCase();
+      }
     }
     return words.join(' ');
   }
