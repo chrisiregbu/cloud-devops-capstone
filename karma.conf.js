f65c8e889d87df743c4737a5e04679b621b54398
chrisiregbu <ciregbu@gmail.com>
2021-11-16 01:28:30 +0000

    Removing reference to ChromeHeadlessCI

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
